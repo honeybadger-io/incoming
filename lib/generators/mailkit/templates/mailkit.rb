a7aeea9810d38f9047acba0f05e918f08dabbf67
Joshua Wood <josh@hintmedia.com>
2013-02-16 14:44:51 -0800

    Remove legacy Rails stuff

--- lib/generators/mailkit/templates/mailkit.rb
+++
@@ -1,4 +0,0 @@
-Mailkit.setup do |config|
-  config.http_post_secret = '6d7e5337a0cd69f52c3fcf9f5af438b1'
-  config.http_post_endpoint = 'http://your-domain.com/emails'
-end
--- lib/tasks/mailkit_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :mailkit do
-#   # Task goes here
-# end
