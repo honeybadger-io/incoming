0f96e762c918edc6499aefa7bc4a6d08affc9234
Joshua Wood <josh@hintmedia.com>
2013-02-18 13:30:07 -0800

    Release 0.1.3

--- lib/incoming.rb
+++ lib/incoming.rb
@@ -2,7 +2,7 @@ require 'mail'
 require 'incoming/strategy'
 
 module Incoming
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   module Strategies
     autoload :SendGrid, 'incoming/strategies/sendgrid'
