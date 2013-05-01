63e24317aae24f33de49477b3ca4bc8eb22057a0
Joshua Wood <josh@hintmedia.com>
2013-04-30 19:25:50 -0700

    Test multiple mailgun recipients /cc #5

--- spec/incoming/strategies/mailgun_spec.rb
+++ spec/incoming/strategies/mailgun_spec.rb
@@ -5,16 +5,12 @@ describe Incoming::Strategies::Mailgun do
 
   before do
     @params = {
-      'recipient' => 'testing@honeybadger.mailgun.org',
-      'sender' => 'josh@honeybadger.io',
-      'from' => 'Joshua Wood <josh@honeybadger.io>',
-      'subject' => 'Matterhorn',
       'body-plain' => "We should do that again sometime.\n> Quoted part",
       'body-html' => '<strong>We should do that again sometime.</strong>\r\n> <em>Quoted part</em>',
       'stripped-text' => 'We should do that again sometime.',
       'stripped-html' => '<strong>We should do that again sometime.</strong>',
       'signature' => 'foo',
-      'message-headers' => [ [ "Received", "by luna.mailgun.net with SMTP mgrt 8786320665205; Wed, 01 May 2013 01:27:31 +0000" ], [ "X-Envelope-From", "<josh@honeybadger.io>" ], [ "Received", "from mail-pa0-f47.google.com (mail-pa0-f47.google.com [209.85.220.47]) by mxa.mailgun.org with ESMTP id 51806f82.7efee87f6a70-in1; Wed, 01 May 2013 01:27:30 -0000 (UTC)" ], [ "Received", "by mail-pa0-f47.google.com with SMTP id bj3so645042pad.6 for <testing@honeybadger.mailgun.org>; Tue, 30 Apr 2013 18:27:29 -0700 (PDT)" ], [ "X-Google-Dkim-Signature", "v=1; a=rsa-sha256; c=relaxed/relaxed; d=google.com; s=20120113; h=x-received:message-id:date:from:user-agent:mime-version:to:subject :x-enigmail-version:content-type:content-transfer-encoding :x-gm-message-state; bh=yZQq1c8wjBl0fZ4Wc/oraMCAG1mZJv5v/hlvyFy+t6A=; b=Vgzjw5DlUSYuw81apkhCqY5j83vdr3Qc6FiEHyiOtNubTqWAEHe8YH3GumyEjwt2lq IeX9/a9YH8cxUIJYCIQ7qHlunXke/IzmypFCjHjyNa4Hcm97KpUgdQ/REc7/7yXjIZ/Z CsE4afdhOpfz1Q7uXYw+LGUHWfF48N82QQVHZp/FyYQcIO0Mk1oVnKgRlQRc/+p7YP+W 8pyybPdTRAg4TvGt0dBoMuKapJbbkHYJyQa1c+JDHWjzGEnX3fdQIYPE8YWzUserEA6/ LLnoUyLVmSwz3A18Ycpoli3RaJkFrdKVwVb1WSMSE/tberAuAhXAs+XJ9uapC+6hMrPO 6FpQ==" ], [ "X-Received", "by 10.68.220.2 with SMTP id ps2mr1421811pbc.51.1367371649529; Tue, 30 Apr 2013 18:27:29 -0700 (PDT)" ], [ "Return-Path", "<josh@honeybadger.io>" ], [ "Received", "from Josh-MacBook-Air.local (c-67-171-132-75.hsd1.or.comcast.net. [67.171.132.75]) by mx.google.com with ESMTPSA id gi2sm816114pbb.2.2013.04.30.18.27.27 for <testing@honeybadger.mailgun.org> (version=TLSv1 cipher=ECDHE-RSA-RC4-SHA bits=128/128); Tue, 30 Apr 2013 18:27:28 -0700 (PDT)" ], [ "Message-Id", "<51806F7B.3080208@honeybadger.io>" ], [ "Date", "Tue, 30 Apr 2013 18:27:23 -0700" ], [ "From", "Joshua Wood <josh@honeybadger.io>" ], [ "User-Agent", "Postbox 3.0.8 (Macintosh/20130427)" ], [ "Mime-Version", "1.0" ], [ "To", "testing@honeybadger.mailgun.org" ], [ "Subject", "Matterhorn" ], [ "X-Enigmail-Version", "1.2.3" ], [ "Content-Type", "text/plain; charset=\"ISO-8859-1\"" ], [ "Content-Transfer-Encoding", "7bit" ], [ "X-Gm-Message-State", "ALoCoQlVbkIehDACf3uTH1cKcdwdTq2pg+C7Wu2rsW0LR56YibUbKCsm1iTs8/iYMK76W9qToNZt" ], [ "X-Mailgun-Incoming", "Yes" ] ].to_json,
+      'message-headers' => [ [ "Received", "by luna.mailgun.net with SMTP mgrt 8747120609393; Wed, 01 May 2013 02:16:10 +0000" ], [ "X-Envelope-From", "<josh@honeybadger.io>" ], [ "Received", "from mail-pa0-f41.google.com (mail-pa0-f41.google.com [209.85.220.41]) by mxa.mailgun.org with ESMTP id 51807ae9.7f7248df4ef0-in3; Wed, 01 May 2013 02:16:09 -0000 (UTC)" ], [ "Received", "by mail-pa0-f41.google.com with SMTP id kq12so669142pab.0 for <testing@honeybadger.mailgun.org>; Tue, 30 Apr 2013 19:16:05 -0700 (PDT)" ], [ "X-Google-Dkim-Signature", "v=1; a=rsa-sha256; c=relaxed/relaxed; d=google.com; s=20120113; h=x-received:message-id:date:from:user-agent:mime-version:to:subject :x-enigmail-version:content-type:content-transfer-encoding :x-gm-message-state; bh=Ba3gj8+xBPQLJTahTfzW6RbWQ/XPgESxkCi2B66PSQg=; b=A2UmxKvNxZAr4yAWPWDfnBELJC8yBfthL7pxex51u3JYLvWdPjhmuJMeaUtSNkBVq1 7nWT8/OrnaXAACeBZc/WFjMNP5BTC+B3Eic4387QseWkqrE17oQSQWBupkzUzW+JLdeD /BEDVhPglhkeOQhtdd3iooRCVAZ++DzDv4zKYOXlNbuvNnDO1D+RVoRkBx6sA+fom8vJ l0HYkqfzVqelt8FKVfQ50J/KFPfItzYuh37Uck54lV6zoE2dGyK5uvNqyEMzNLPnd1PI XC1t8G/61I8lGl0MMdju6KD31flClhcmEr282c1YMXzGTWtgD22t39snAk9sI0Ce31Dv dGlw==" ], [ "X-Received", "by 10.66.220.10 with SMTP id ps10mr2456146pac.117.1367374564998; Tue, 30 Apr 2013 19:16:04 -0700 (PDT)" ], [ "Return-Path", "<josh@honeybadger.io>" ], [ "Received", "from Josh-MacBook-Air.local (c-67-171-132-75.hsd1.or.comcast.net. [67.171.132.75]) by mx.google.com with ESMTPSA id ya4sm939634pbb.24.2013.04.30.19.16.02 for <multiple recipients> (version=TLSv1 cipher=ECDHE-RSA-RC4-SHA bits=128/128); Tue, 30 Apr 2013 19:16:04 -0700 (PDT)" ], [ "Message-Id", "<51807AE0.2020403@honeybadger.io>" ], [ "Date", "Tue, 30 Apr 2013 19:16:00 -0700" ], [ "From", "Joshua Wood <josh@honeybadger.io>" ], [ "User-Agent", "Postbox 3.0.8 (Macintosh/20130427)" ], [ "Mime-Version", "1.0" ], [ "To", "testing@honeybadger.mailgun.org, Wood Joshua <joshuawood@gmail.com>" ], [ "Subject", "Matterhorn" ], [ "X-Enigmail-Version", "1.2.3" ], [ "Content-Type", "text/plain; charset=\"ISO-8859-1\"" ], [ "Content-Transfer-Encoding", "7bit" ], [ "X-Gm-Message-State", "ALoCoQmejbb0rnWD00BBdqBXLoONkEpAYyufP56rBpr80ZKyDXNN1/t/O9MRqscRmoLVPrE9tu+G" ], [ "X-Mailgun-Incoming", "Yes" ] ].to_json,
       'attachment-count' => '2',
       'attachment-1' => stub(:original_filename => 'foo.txt', :read => 'hello world'),
       'attachment-2' => {
@@ -37,9 +33,10 @@ describe Incoming::Strategies::Mailgun do
 
       it { should be_a Mail::Message }
 
-      its('to.first') { should eq @params['recipient'] }
-      its('from.first') { should eq @params['sender'] }
-      its('subject') { should eq @params['subject'] }
+      its('to') { should include 'testing@honeybadger.mailgun.org' }
+      its('to') { should include 'joshuawood@gmail.com' }
+      its('from.first') { should eq 'josh@honeybadger.io' }
+      its('subject') { should eq 'Matterhorn' }
       its('body.decoded') { should eq @params['body-plain'] }
       its('text_part.body.decoded') { should eq @params['body-plain'] }
       its('html_part.body.decoded') { should eq @params['body-html'] }
--- spec/incoming/strategies/sendgrid_spec.rb
+++ spec/incoming/strategies/sendgrid_spec.rb
@@ -6,16 +6,9 @@ describe Incoming::Strategies::SendGrid do
       'SPF' => 'pass',
       'charsets' => '{"from": "UTF-8", "subject": "UTF-8", "text": "ISO-8859-1", "to": "UTF-8"}',
       'dkim' => 'none',
-      'envelope' => JSON.generate({
-        'to' => ['testing@sendgrid.honeybadger.io'],
-        'from' => 'josh@honeybadger.io'
-      }),
-      'from' => 'Joshua Wood <josh@honeybadger.io>',
       'headers' => "Received: by 127.0.0.1 with SMTP id IQklCWgXx9 Tue, 30 Apr 2013 20:38:01 -0500 (CDT)\r\nReceived: from mail-da0-f49.google.com (mail-da0-f49.google.com [209.85.210.49]) by mx3.sendgrid.net (Postfix) with ESMTPS id 15F7314F48C1 for <testing@sendgrid.honeybadger.io>; Tue, 30 Apr 2013 20:38:01 -0500 (CDT)\r\nReceived: by mail-da0-f49.google.com with SMTP id t11so483187daj.22 for <testing@sendgrid.honeybadger.io>; Tue, 30 Apr 2013 18:38:00 -0700 (PDT)\r\nX-Google-DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=google.com; s=20120113; h=x-received:message-id:date:from:user-agent:mime-version:to:subject :x-enigmail-version:content-type:content-transfer-encoding :x-gm-message-state; bh=EVfAHeUMDygbJe0SkMWJHjgXGjtiTLZnMQbyWqzsrCY=; b=Y5UefHXzM6KLBH/TQejxrMlJ3sPJThU1DS0/uNLWUZkjeG4wAwOEfE8XuesBL7SUF1 0G6OAlCqpnZMl8F8iitlt2iuuW1+5NqOiBIsBnYFJ5Y5EP8XiPivUaigWZLtGfZH7sTY 2tNQ74UfVSzxx5gyjajkzuT8qNa+zYiNVL14wQy2HWu/FxTUBGy5VRko7hVXdJIck7fn 4E+4p34f1j2CGPSYXg4qDgBwE4m4nllQqu0/k/xHB66+iJl05uMZ2BUGYoRQI2aE4jC1 GlbxQhUU5I/kefIjrdm1wuf92sarKTtFA6kcao1Z3pgExARbcpuXTdgIsgq/WUBf424U JCnQ==\r\nX-Received: by 10.66.177.46 with SMTP id cn14mr2412679pac.4.1367372280251; Tue, 30 Apr 2013 18:38:00 -0700 (PDT)\r\nReceived: from Josh-MacBook-Air.local (c-67-171-132-75.hsd1.or.comcast.net. [67.171.132.75]) by mx.google.com with ESMTPSA id ef4sm782898pbd.38.2013.04.30.18.37.57 for <testing@sendgrid.honeybadger.io> (version=TLSv1 cipher=ECDHE-RSA-RC4-SHA bits=128/128); Tue, 30 Apr 2013 18:37:59 -0700 (PDT)\r\nMessage-ID: <518071F1.5010108@honeybadger.io>\r\nDate: Tue, 30 Apr 2013 18:37:53 -0700\r\nFrom: Joshua Wood <josh@honeybadger.io>\r\nUser-Agent: Postbox 3.0.8 (Macintosh/20130427)\r\nMIME-Version: 1.0\r\nTo: testing@sendgrid.honeybadger.io\r\nSubject: Matterhorn\r\nX-Enigmail-Version: 1.2.3\r\nContent-Type: text/plain; charset=ISO-8859-1\r\nContent-Transfer-Encoding: 7bit\r\nX-Gm-Message-State: ALoCoQkHpc+fg75T6PPAtGWRMs8lDGZtyRSNSt4DRh7Gm29YjOKj4VThC/EdJAiqrqkuRX1HKyA6",
-      'subject' => 'Matterhorn',
       'text' => 'We should do that again sometime.',
       'html' => '<strong>We should do that again sometime</strong>',
-      'to' => 'testing@sendgrid.honeybadger.io',
       'attachments' => '2',
       'attachment1' => stub(:original_filename => 'hello.txt', :read => 'hello world'),
       'attachment2' => {
@@ -36,9 +29,9 @@ describe Incoming::Strategies::SendGrid do
 
     it { should be_a Mail::Message }
 
-    its('to.first') { should eq 'testing@sendgrid.honeybadger.io' }
-    its('from.first') { should eq 'josh@honeybadger.io' }
-    its('subject') { should eq @params['subject'] }
+    its('to') { should include 'testing@sendgrid.honeybadger.io' }
+    its('from') { should include 'josh@honeybadger.io' }
+    its('subject') { should eq 'Matterhorn' }
     its('body.decoded') { should eq @params['text'] }
     its('text_part.body.decoded') { should eq @params['text'] }
     its('html_part.body.decoded') { should eq @params['html'] }
